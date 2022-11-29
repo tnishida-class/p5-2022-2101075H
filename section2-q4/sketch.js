64b1d744a5716d74e440cd39fbd8a87cd441ca77
waka <114969445+2101075H@users.noreply.github.com>
2022-11-29 16:00:49 +0900

    section2-q4

--- section2-q4/sketch.js
+++ section2-q4/sketch.js
@@ -1,14 +1,20 @@
 // ギリシャ国旗
 function setup() {
-  const blue = color(0, 51, 160);
-  createCanvas(270, 180);
-  // noStroke();
+  const blue = color(100, 149, 237);
+  createCanvas(540, 360);
+  noStroke();
   background(255);
 
   let d = height / 9; // 縞1本の太さ
 
   for(let i = 0; i < 9; i++){
     // BLANK[1] (hint: 縞の色を交互に変えるには2で割った余りを使おう)
+    if(i % 2 == 0){
+      fill(blue)
+    }
+    else{
+      fill(255)
+    };
     rect(0, i * d, width, (i + 1) * d);
   }
 
@@ -17,5 +23,6 @@ function setup() {
   rect(0, 0, size, size);
 
   fill(255);
-  // BLANK[2] (hint: 白い十字を描くには rect を二つ描こう)
+  rect(0,size * 0.4,size,d)
+  rect(size * 0.4,0,d,size)
 }
